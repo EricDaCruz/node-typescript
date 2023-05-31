46f47623a1e7773ad1c9c8c019fcd430bb35f3a6
Eric Da Cruz <rafaeleric40@gmail.com>
2023-05-31 20:40:42 -0300

    feat: CityController - Create

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,19 +1,15 @@
 import { Request, Response, Router } from "express";
 import { StatusCodes } from "http-status-codes";
 
+import { CitiesController } from "../../controllers";
+
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
   return res.send("Hello Dev!");
 });
 
-router.post("/test/:id", (req: Request, res: Response) => {
-  const name = req.body.name;
-  const id = req.params.id;
-  const message = req.query.message;
-  return res
-    .status(StatusCodes.CREATED)
-    .json({ message: `Hello ${name} your id is ${id}. You say ${message}` });
-});
+router.post("/cities", CitiesController.create);
+
 
 export { router };
