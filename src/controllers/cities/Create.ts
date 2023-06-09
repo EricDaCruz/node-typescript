62394efd751b48bc3ca5325b1dbd2e3caf30001b
Eric Da Cruz <rafaeleric40@gmail.com>
2023-06-09 16:57:15 -0300

    feat: Validating body request

--- src/controllers/cities/Create.ts
+++ src/controllers/cities/Create.ts
@@ -1,13 +1,30 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface ICity {
   name: string;
 }
 
-export const create = (req: Request<{}, {}, ICity>, res: Response) => {
-  const data = req.body;
+const bodyValidation: yup.Schema<ICity> = yup.object().shape({
+  name: yup.string().required().min(3),
+});
 
-  console.log(data);
+export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
+  const data = req.body;
+  let validatedData: ICity | undefined = undefined;
+
+  try {
+    validatedData = await bodyValidation.validate(data);
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: yupError.message,
+      },
+    });
+  }
 
   return res.send("Create a new city");
 };
